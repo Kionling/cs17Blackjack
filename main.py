8e6127ffe0af735ee0cc8df4d32dd3019d09a42c
Daniel Jauregui <djvdjv209@gmail.com>
2024-03-20 23:17:28 -0700

    Created function that declares values of cards and their suits. The function then iterates through deck and shuffles

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 import random
+
+
+def create_deck(): 
+    suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+    values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+    deck = [{'suit': suit, 'value': value} for suit in suits for value in values]
+    random.shuffle(deck)
+    return deck
+
+    
