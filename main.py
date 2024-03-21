820053bf6fcd9a2694ffb55db941275162130105
Daniel Jauregui <djvdjv209@gmail.com>
2024-03-20 23:22:32 -0700

    defined the benefit of having an ace on a hand that has the hand value less than 21. Added hand parameter that takes in the current hand of player or dealer

--- main.py
+++ main.py
@@ -9,3 +9,18 @@ def create_deck():
     return deck
 
     
+def hand_value(hand):
+    value = 0
+    ace_count = 0
+    for card in hand:
+        if card['value'] in ['Jack', 'Queen', 'King']:
+            value += 10
+        elif card['value'] == 'Ace':
+            ace_count += 1
+        else:
+            value += int(card['value'])
+    while ace_count > 0 and value + 10 <= 21:
+        value += 10
+        ace_count -= 1
+    return value
+
